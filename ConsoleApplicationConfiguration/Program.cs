8d1e4ac878749e66f2cf88bd9e1b27c0321810ea
Chris <zc850926@163.com>
2016-12-23 14:02:20 +0800

    Init c# with app.config file
    1. add refenrence system.configuration
    2. add config file General -->application Configuration file
    3. edit config file
    4. call ConfigurationManager

---
+++ ConsoleApplicationConfiguration/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplicationConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string a=Console.ReadLine();
+            //switch (a)
+            //{
+            //    case "1":
+            //        Console.WriteLine(1);
+            //        break;
+            //    case "2":
+            //        Console.WriteLine(2);
+            //        break;
+            //    default :
+            //        Console.WriteLine(a);
+            //        break;
+            //}
+            //Console.ReadKey();
+
+            //NameValueCollection appSettings = System.Configuration.ConfigurationManager.AppSettings;
+            //string name = appSettings["name"];
+            //appSettings.Add("w", "b");
+
+            //Console.WriteLine(name);
+            //Console.ReadKey();
+            //read and update on exe.config not this app.config
+            Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            Console.WriteLine(config.AppSettings.Settings["name"].Value);
+            config.AppSettings.Settings["name"].Value = "Chen";
+            Console.WriteLine(config.AppSettings.Settings["name"].Value);
+            Console.ReadKey();
+            //app.Settings["name"].Value = "Zhao";
+
+            config.Save();
+            //Console.WriteLine(config.AppSettings.Settings["name"].Value);
+            //Console.ReadKey();
+            //System.Configuration.ConfigurationManager.RefreshSection("appSettings");
+        }
+    }
+}

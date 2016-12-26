43f23d096e91fa1dfaabe729793decf821a17b31
Chris <zc850926@163.com>
2016-12-26 11:00:00 +0800

    try to call DMS webservice API

--- ConsoleApplicationConfiguration/Program.cs
+++ ConsoleApplicationConfiguration/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApplicationConfiguration.ServiceReference1;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Configuration;
@@ -34,16 +35,21 @@ namespace ConsoleApplicationConfiguration
             //Console.ReadKey();
             //read and update on exe.config not this app.config
             Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
-            Console.WriteLine(config.AppSettings.Settings["name"].Value);
-            config.AppSettings.Settings["name"].Value = "Chen";
-            Console.WriteLine(config.AppSettings.Settings["name"].Value);
-            Console.ReadKey();
+            string DMSAPI_USER = config.AppSettings.Settings["DMSAPI_USER"].Value;
+            string DMSAPI_PASSWORD = config.AppSettings.Settings["DMSAPI_Password"].Value;
+            //Console.WriteLine(config.AppSettings.Settings["name"].Value);
+            //config.AppSettings.Settings["name"].Value = "Chen";
+            //Console.WriteLine(config.AppSettings.Settings["name"].Value);
+            //Console.ReadKey();
             //app.Settings["name"].Value = "Zhao";
 
-            config.Save();
+            //config.Save();
             //Console.WriteLine(config.AppSettings.Settings["name"].Value);
             //Console.ReadKey();
             //System.Configuration.ConfigurationManager.RefreshSection("appSettings");
+            IssueServiceClient obj = new IssueServiceClient();
+            var aaa = obj.GetIssueForShiroyagiJira(DMSAPI_USER, DMSAPI_PASSWORD, "", "Convergence Jira", "").First();
+            Console.ReadKey();
         }
     }
 }
